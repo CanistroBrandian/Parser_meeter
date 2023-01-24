a9d50847525d0c072be370844e749618326b98c7
canistra_brandian <brovkokosty@gmail.com>
2023-01-24 20:41:06 +0300

    1.0

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ import numpy as np
 import pandas as pd
 from pandas import ExcelWriter
 
+def to_excel(dataFrame):
+    data = pd.DataFrame(dataFrame,columns=['Data','Place','Link_place','Event','Link_event','Ganre','Time_start','Price'])
+    with pd.ExcelWriter('output.xlsx') as writer:
+        data.to_excel(writer)
+
 user = fake_useragent.UserAgent().random
 header = {'user-agent': user}
-
 store_data =[]
 local_variable=[]
 
@@ -31,38 +35,45 @@ for event in all_event_list:
     
     for place in rows_events:
         
-        name_place = place.find('a', class_='schedule__place-link link') 
+        name_place = place.find('a', class_='schedule__place-link link')#
         
-        if name_place:
+        if name_place:#
             link_place = name_place.get('href')
             name_place = name_place.text
             prev_name_place = name_place
         else:
             name_place = prev_name_place
-
+      
+        name_event = place.find('a', class_='schedule__event-link link')#
+        link_event = name_event.get('href')       #
+        name_ganre = place.find('a', class_='schedule__event-dscr text-black-light')#
         
-        name_event = place.find('a', class_='schedule__event-link link')
-        link_event = name_event.get('href')       
-        name_ganre = place.find('a', class_='schedule__event-dscr text-black-light')
-        
-        if name_ganre:
+        if name_ganre:#
             name_ganre = name_ganre.text
             
         time_start_arr = place.findAll('a', \
-                                       class_='schedule__seance-time schedule__seance--buy js-buy-ticket')
+                                       class_='schedule__seance-time schedule__seance--buy js-buy-ticket')#
         price_event_arr = place.findAll('span', \
-                                        class_='seance-price')
-        time_start = ', '.join([time.text \
-                                for time in time_start_arr])
+                                        class_='seance-price')#
+        time_start = ', '.join([time.text.strip() \
+                                for time in time_start_arr])#
         price_event = ', '.join([price.text \
-                                for price in price_event_arr])
+                                for price in price_event_arr])#
         
+        #форматирование строк
+        data_event=' '.join(data_event.split()).split(',')[0]
+        name_event_format = name_event.text
+        name_event_format = ' '.join(name_event_format.split()).split(',')[0]
+        
+        #Добавление значений в массив 
         local_variable.append([data_event,\
-                        name_place,link_place,\
-                        name_event.text,link_event,\
+                        name_place.strip() ,link_place,\
+                        name_event_format,link_event,\
                         name_ganre,time_start,\
                         price_event])
     store_data += local_variable
+    
     local_variable = []
 df = pd.DataFrame(store_data,columns=['Data','Place','Link_place','Event','Link_event','Ganre','Time_start','Price'])
+to_excel(store_data)
 
